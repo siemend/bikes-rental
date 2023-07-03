a33eb1a166212da82b68d8a142580d8a262e9e6b
weldplaner <weldplaner@gmail.com>
2023-07-04 01:14:21 +0300

    2

--- src/components/pages/Pages.jsx
+++ src/components/pages/Pages.jsx
@@ -15,7 +15,7 @@ import {Signin} from "../sign_in/Signin"
 import {Report} from "../report/Report"
 
 
-function Pages() {
+export default function Pages() {
   const [data, setData] = useState([]);
   const [approved, setApproved] = useState([]);
   const [email, setEmail] = useState("");
@@ -62,14 +62,15 @@ function Pages() {
       <Router>
         <Header admin={admin} setAdmin={setAdmin} />
         <Switch>
-          <Route path="/" element={<Home />}></Route>
+          <Route exact path="/" element={<Home />}></Route>
+        </Switch> 
+        <Switch>
+          <Route exact path="auth/sign_up" element={<Signup />}></Route>
         </Switch>
         <Switch>
-          <Route path="auth/sign_up" element={<Signup />}></Route>
-        </Switch>
-        <Switch>
+          <Route exact path="/sign_in"><div>Sign in</div></Route>
           <Route
-            path="auth/sign_in"
+            exact path="/sign_in"
             element={
               <Signin
                 admin={admin}
@@ -86,36 +87,27 @@ function Pages() {
               />
             }
           ></Route>
-          <Route
-            path="public/report"
-            element={<Report admin={admin} />}
+          <Route exact path="public/report" element={<Report admin={admin} />}></Route>
+          <Route path="/officers" element={<Officers approved={approved} setApproved={setApproved} />}
+          ></Route>
+          <Route path="/cases/" element={<Messages approved={approved} setApproved={setApproved} />}
+          ></Route>
+          <Route path="/officers/:id" element={<Officers approved={approved} setApproved={setApproved} />}
           ></Route>
           <Route
-            path="/officers"
-            element={
-              <Officers approved={approved} setApproved={setApproved} />
-            }
-          ></Route>
-          <Route
-            path="/cases/"
-            element={<Messages approved={approved} setApproved={setApproved} />}
-          ></Route>
-          <Route
-            path="/officers/:id"
-            element={
-              <Officers approved={approved} setApproved={setApproved} />
-            }
-          ></Route>
-          <Route
-            path="/cases/:id"
+            exact path="/cases/:id"
             element={<Messages approved={approved} setApproved={setApproved} />}
           ></Route>
         </Switch>
         <Switch>
-          <Route exact path='/' component={Home} />
-          <Route exact path='/using' component={Using} />
-          <Route exact path='/chooseplan' component={ChoosePlan} />
-          <Route exact path='/contact' component={Contact} />
+          <Route exact path='/' component={Home}></Route>
+          <Route exact path='/signin' component={Signin}></Route>
+          <Route exact path='/signup' component={Signup}></Route>
+          <Route exact path='/using' component={Using}></Route>
+          <Route exact path='/report' component={Report}></Route>
+          <Route exact path='/chooseplan' component={ChoosePlan}></Route>
+          <Route exact path='/contact' component={Contact}></Route>
+
         </Switch>
         <Footer />
       </Router>
@@ -123,4 +115,3 @@ function Pages() {
   );
 }
 
-export default Pages
